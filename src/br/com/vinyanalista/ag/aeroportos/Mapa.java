e35bbcf431b5ded63271e6b251d4b7a8ffc75910
Antônio Vinícius <vinyanalista@gmail.com>
2015-01-18 22:46:24 -0300

    Esqueleto do problema dos aeroportos

---
+++ src/br/com/vinyanalista/ag/aeroportos/Mapa.java
@@ -0,0 +1,31 @@
+package br.com.vinyanalista.ag.aeroportos;
+
+import br.com.vinyanalista.ag.DefinicaoDoIndividuo;
+
+public class Mapa implements DefinicaoDoIndividuo<Mapa> {
+
+	@Override
+	public Mapa cruzar(Mapa outroMapa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int fitness() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int fitnessDesejado() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void mutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/br/com/vinyanalista/ag/aeroportos/ProblemaDosAeroportos.java
@@ -0,0 +1,13 @@
+package br.com.vinyanalista.ag.aeroportos;
+
+import br.com.vinyanalista.ag.*;
+
+public class ProblemaDosAeroportos extends AlgoritmoGenetico<Mapa> {
+
+	@Override
+	protected double probabilidadeDeMutacao() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
